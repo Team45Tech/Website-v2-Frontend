7836068948912c46ee8e561f3c3b2015108f3a00
Liz <93419255+Kiana315@users.noreply.github.com>
2023-11-01 03:57:08 -0600

    Create hero of home page

---
+++ app/components/HomePage/Hero.tsx
@@ -0,0 +1,16 @@
+// Hero in Home Page
+
+import React, { useRef } from 'react';
+import './hero.css'
+
+export default function Hero() {
+    return (
+        <div className='Hero'>
+            <div className='background'>
+                <img src='/CISD-BG.jpg' alt='Background image'>
+
+                </img>
+            </div>
+        </div>
+    )
+}

79cd3db4e4c0f247f9fee3debe2042c990302b82
Liz <93419255+Kiana315@users.noreply.github.com>
2023-11-01 03:58:51 -0600

    Initialize pages

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+// _app.tsx
+// Do not touch this file!!!!!!!!!!!
+
+import '../app/styles/globals.css';
+import Layout from '../app/components/layout';
+import type { AppProps } from 'next/app';  // Import AppProps here
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
